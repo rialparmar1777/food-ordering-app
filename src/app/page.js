38550e6a07d0d6113e5671c2df2bce0e29e87c18
Rial <rialparmar007@gmail.com>
2025-01-28 16:38:44 -0800

    Hero page setup for main content

--- src/app/page.js
+++ src/app/page.js
@@ -1,11 +1,13 @@
 import Image from "next/image";
 import Link from "next/link";
 import Header from "@/components/layout/Header";
+import Hero from "@/components/layout/Hero";
 
 export default function Home() {
   return (
     <>
     <Header />
+    <Hero />
     </>
   )
 }
---
+++ src/components/layout/Hero.js
@@ -0,0 +1,12 @@
+import Link from "next/link"
+
+export default function Hero() {
+    return(
+        <>
+        <section>
+            <h1>Pizza</h1>
+            <p>Always Fresh</p>
+        </section>
+        </>
+    )
+}
