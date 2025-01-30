00bb0bd4c1c2ec101873942d3a3fed9a52ad0573
Rial <rialparmar007@gmail.com>
2025-01-29 23:54:10 -0800

    few changes in HomeMenu

--- src/components/layout/HomeMenu.js
+++ src/components/layout/HomeMenu.js
@@ -3,21 +3,20 @@ import Image from "next/image";
 
 export default function HomeMenu() {
   return (
-    <section>
-        <div className="relative">
-            <div className="h-48 w-48  relative">
-                <Image src={'/sallad1.png'} layout={'fill'} objectFit={'contain'} alt={'sallad'} />
-            </div>
-            <div className="h-48 w-48 relative">
-                <Image src={'/sallad2.png'} layout={'fill'} objectFit={'contain'} alt={'sallad'} />
-            </div>
+    <section className="relative">
+      <div className="flex justify-between items-center">
+        <div className="h-48 w-48 relative">
+          <Image src={'/sallad1.png'} layout={'fill'} objectFit={'contain'} alt={'sallad'} />
         </div>
-      <div className={"text-center"}>
-        <div>
+        <div className="text-center">
           <h3 className="uppercase text-gray-700 font-semibold leading-4">Check Out</h3>
           <h2 className="text-primary font-bold text-4xl italic">Menu</h2>
         </div>
+        <div className="h-48 w-48 relative">
+          <Image src={'/sallad2.png'} layout={'fill'} objectFit={'contain'} alt={'sallad'} />
+        </div>
       </div>
+      
     </section>
   );
 }
