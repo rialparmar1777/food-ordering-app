d95ba016999d9afa3defd2cc19bd9406038c5a72
Rial <rialparmar007@gmail.com>
2025-01-29 18:58:01 -0800

    working on Hero page content

--- src/components/layout/Hero.js
+++ src/components/layout/Hero.js
@@ -1,12 +1,31 @@
-import Link from "next/link"
+import Link from "next/link";
+import Image from "next/image";
 
 export default function Hero() {
-    return(
-        <>
-        <section>
-            <h1>Pizza</h1>
-            <p>Always Fresh</p>
-        </section>
-        </>
-    )
+  return (
+    <>
+      <section className="grid grid-cols-2">
+        <div>
+          <h1 className="text-4xl font-semibold">Everything is better with a Pizza</h1>
+          <p className="my-4 text-gray-600">
+            Pizza is the missing piece that makes every day complete, a simple
+            yet delicious joy in life
+          </p>
+          <div>
+            <button>Order Now</button>
+            <button>Learn More</button>
+          </div>
+        </div>
+
+        <div className="relative">
+          <Image
+            src={"/pizza.png"}
+            layout={"fill"}
+            objectFit={"contain"}
+            alt={"pizza"}
+          />
+        </div>
+      </section>
+    </>
+  );
 }
