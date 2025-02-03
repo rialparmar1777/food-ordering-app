4807d8330128c190d364c36a9a14c207e992c258
Rial <rialparmar007@gmail.com>
2025-02-02 23:04:20 -0800

    unfinished register page without proper styling

--- src/app/register/page.js
+++ src/app/register/page.js
@@ -1,9 +1,22 @@
+import Image from "next/image";
+
 export default function RegisterPage() {
     return(
         <section className="mt-20">
-            <h1 className="text-center text-primary text-4xl">
+            <h1 className="text-center text-primary text-4xl mb-4">
                 Register
             </h1>
+            <form className="block max-w-xs mx-auto">
+                <input type="email" placeholder="email" />
+                <input type="password" placeholder="password" />
+                <button type="submit">Register</button>
+                <div className="my-4 text-center text-gray-600">or login with other provider</div>
+                <button className="flex gap-4 justify-center">
+                    <Image src={'/google.png'} alt={''} width={32} height={32} />
+                    Login with Google</button>
+
+            </form>
+            
         </section>
     );
 }
