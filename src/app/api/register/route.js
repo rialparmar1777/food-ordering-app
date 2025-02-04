3dc2d68241922812147040fbf9b77949bb8f8ba4
Rial <rialparmar007@gmail.com>
2025-02-03 17:55:46 -0800

    solving few server-client errors

--- src/app/api/register/route.js
+++ src/app/api/register/route.js
@@ -1,4 +1,14 @@
-export function POST(req) {
-    
-    return Response.json('You are registered!');
+"use server"
+import mongoose from "mongoose";
+import { User } from "../models/User";
+
+export async function POST(req) {
+     // Ensure this runs on the server
+
+    const body = await req.json();
+
+    await mongoose.connect(process.env.MONGO_URL);
+    const createdUser = await User.create(body);
+
+    return Response.json(createdUser);
 }
